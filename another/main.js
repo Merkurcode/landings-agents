eba8fa6ee730eb297f0731a6a2bb2fb2a60a842f
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2025-11-10 18:43:18 -0600

    chore: add another/main.js

---
+++ another/main.js
@@ -0,0 +1,20 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        console.log('Formulario enviado');
+    });
+
+(function(d,t){
+      var BASE_URL="https://app.chatwoot.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"vdTKwXXuTyNw7QujBGyQ7K8n",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
