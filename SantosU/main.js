4682cb4dc151048f5858ba47162b4ae16e36263e
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2025-11-11 10:49:21 -0600

    chore: add SantosU/main.js

---
+++ SantosU/main.js
@@ -0,0 +1,25 @@
+// Smooth scroll for internal links
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+
+(function(d,t){
+      var BASE_URL="https://agents-test.merkur.la";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"hcoGu7a1u7M57Gcqy6XptQzC",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
