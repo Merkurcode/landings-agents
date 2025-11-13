ed52c811684a39bd04eb0e77347297e24f368bef
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2025-11-13 08:18:38 -0600

    chore: add Comics/main.js

--- Comics/main.js
+++ Comics/main.js
@@ -1,11 +1,5 @@
-document.querySelectorAll('.cta-button').forEach(button => {
-            button.addEventListener('click', () => {
-                console.log('CTA clicked');
-            });
-        });
-
 (function(d,t){
-      var BASE_URL="https://agents-test.merkur.la";
+      var BASE_URL="https://agents.merkur.la";
       var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
       g.src=BASE_URL+"/packs/js/sdk.js";
       g.async=true;
