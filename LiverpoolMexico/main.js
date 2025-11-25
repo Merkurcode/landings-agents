f3be62e4fe7262eb4c55dd9ffeea5e6e8044f9b4
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2025-11-25 10:12:55 -0600

    chore: add LiverpoolMexico/main.js

---
+++ LiverpoolMexico/main.js
@@ -0,0 +1,15 @@
+(function(d,t){
+      var BASE_URL="https://agents-test.merkur.la";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"LrBnhRXbutbHSxeFgCcsBTZB",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
