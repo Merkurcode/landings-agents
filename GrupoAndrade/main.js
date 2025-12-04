bf6d94cb894a69e11ce361998e596ee4b26c6bee
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2025-12-04 11:01:39 -0600

    chore: add GrupoAndrade/main.js

--- GrupoAndrade/main.js
+++ GrupoAndrade/main.js
@@ -1,12 +1,3 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-
 (function(d,t){
       var BASE_URL="https://console-test.nauto.la";
       var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
