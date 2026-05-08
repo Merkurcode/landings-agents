5fa5b4c3689b75abc805f2c76d778459bca1feb7
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2026-05-08 13:07:23 -0600

    chore: add shikimoto/main.js

---
+++ shikimoto/main.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+
+(function(d,t){
+      var BASE_URL="https://console-test.nauto.la";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      g.async=true;
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        if (window.chatwootSDK) {
+          window.chatwootSDK.run({
+            websiteToken:"RqQBEnbMfJprDjeVbm9ikANB",
+            baseUrl:BASE_URL
+          });
+        }
+      };
+    })(document,"script");
